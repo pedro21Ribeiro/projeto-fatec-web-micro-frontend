aa4e229986520359e7c50d9223653fafd21f76d0
pedro21ribeiro <pedr21ribeiro7@gmail.com>
2025-06-13 14:01:41 -0300

    Ultimos arquivos de estudo adicinoados

--- Go+htmx/main.go
+++ Go+htmx/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"html/template"
 	"io"
+	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"pedro21ribeiro.com/repository"
 )
 
 type Templates struct {
@@ -27,15 +28,19 @@ func newTemplate() *Templates{
 	}
 }
 
+var id int = 0
 type Contact struct{
 	Name string
 	Email string
+	Id int
 }
 
 func newContact(name, email string) Contact {
+	id++
 	return Contact{
 		Name: name,
 		Email: email,
+		Id: id,
 	}
 }
 
@@ -49,13 +54,35 @@ type Data struct{
 	Contacts Contacts
 }
 
-type formData struct {
+func (d *Data) indexOf(id int) int {
+	for i, contact := range d.Contacts {
+		if(contact.Id == id){
+			return i
+		}
+	}
+
+	return -1
+}
+
+type FormData struct {
 	Values map[string]string
 	Errors map[string]string
 }
 
-func newFormData() formData{
-	return formData{
+type Page struct{
+	Data Data
+	Form FormData
+}
+
+func newPage() Page{
+	return Page{
+		Data: newData(),
+		Form: newFormData(),
+	}
+}
+
+func newFormData() FormData{
+	return FormData{
 		Values: make(map[string]string),
 		Errors: make(map[string]string),
 	}
@@ -86,36 +113,54 @@ func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 
-	data := newData() 
+	page := newPage()
 
 	e.Renderer = newTemplate()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", data)
+		return c.Render(200, "index", page)
 	})
 
 	e.POST("/contacts", func(c echo.Context) error {
 		name := c.FormValue("name")
 		email := c.FormValue("email")
 
-		if data.hasEmail(email) {
-			return c.Render(400, "form", data)
+		if page.Data.hasEmail(email) {
+			formData := newFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+
+			formData.Errors["email"] = "Email already exists"
+
+			return c.Render(422, "form", formData)
 		}
 
-		data.Contacts = append(data.Contacts, newContact(name, email))
-		return c.Render(200, "display", data)
+		contact := newContact(name, email)
+		page.Data.Contacts = append(page.Data.Contacts, contact)
+		c.Render(200, "form", newFormData())
+		return c.Render(200, "oob-contact", contact)
 	})
 
-	e.POST("/findUser", func(c echo.Context) error {
-		user, err := repository.GetFirstUser(); if err != nil {
-			c.Get()
-			return c.JSON(400, ErrorResponse{
-				Message: err.Error(),
-			})
+	e.DELETE("/contacts/:id", func(c echo.Context) error {
+		time.Sleep(1 * time.Second)
+		idStr := c.Param("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			return c.String(400, "Invalid Id")
 		}
 
-		return c.JSON(200, user)
+		index := page.Data.indexOf(id)
+		if index == -1 {
+			return c.String(404, "Contact Not found")	
+		}
+
+		page.Data.Contacts = append(page.Data.Contacts[:index], page.Data.Contacts[index+1:]...)
+
+		return c.NoContent(200)
 	})
+
+	e.Static("/images", "images")
+	e.Static("/css", "css")
 
 	e.Logger.Fatal(e.Start(":42069"))
 }
