5ef3f696c6bda5e7a66a54760e8a98707e85da02
Pedro Ribeiro <pribeiro@ciandt.com>
2025-06-13 08:15:15 -0300

    Adicionado o .gitignore e começando a implementar um db

---
+++ Go+htmx/conector/database.go
@@ -0,0 +1,13 @@
+package conector
+
+import (
+  "gorm.io/driver/postgres"
+  "gorm.io/gorm"
+)
+
+
+func GetDatabase() (*gorm.DB, error){
+	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	return db, err
+}
